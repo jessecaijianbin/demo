6fc08193b0b2b5d55e34d2fa751473ab875d23c8
caijianbin <1101859469@qq.com>
2018-06-29 14:21:00 +0800

    modify

--- js/chart/dataCompare.js
+++ js/chart/dataCompare.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $(".dataAndLongRange span").click(function(){
         $(this).addClass("selected").siblings("span").removeClass("selected").parent().siblings(".dataAndLongRangeCon").children("div").eq($(this).index()).addClass("selected").siblings().removeClass("selected");
         _initChart($(this).index())
